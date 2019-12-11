7ac90a4cdd37840262ecbeb322e79c44e7d277db
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2019-12-11 18:36:23 +0100

    INIT

--- push_swap.c
+++ push_swap.c
@@ -23,19 +23,32 @@ void	ft_putstr_err(char *err)
 
 int	main(int ac, char **av)
 {
+	int	*pile_a;
+	int	*pile_b;
+	int	i;
+	int	j;
+	
 	if (ft_is_valid(ac, av) == 0)
 	{
 		ft_putstr_err("Error\n");
 		exit(-1);
 	}
-	int	i;
+	if (!(pile_a = (int *)malloc(sizeof(int) * (ac - 1))))
+		exit(-1);
+	if (!(pile_b = (int *)malloc(sizeof(int) * (ac - 1))))
+		exit(-1);
 
 	i = 1;
+	j = 0;
 	while (i < ac)
+		pile_a[j++] = ft_atoi(av[i++]);
+	j = 0;
+	while (j < ac - 1)
 	{
-		printf("%s\n", av[i]);
+		printf("%d\n", pile_a[j]);
 		fflush(stdout);
-		i++;
-	}
+		j++;
+
+	}	
 	return (0);
 }
