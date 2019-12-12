3a6675cf8e6b12719674da432531022b094432f0
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2019-12-12 18:25:16 +0100

    ADDING start of quicksort

---
+++ ft_sort.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcoloma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/12 15:54:40 by dcoloma           #+#    #+#             */
+/*   Updated: 2019/12/12 15:54:48 by dcoloma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	ft_quick_sort(int *tab, int len)
+{
+	
+}
+
+void		ft_sort(int *tab, int len, int sort)
+{
+	if (sort == QUICK_SORT)
+		ft_quick_sort(tab, len);
+}
