d7cd61e131e4e6ae3b2e0c0bcec18289282de8d4
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2019-12-18 16:36:24 +0100

    ADD push

--- ft_push.c
+++ ft_push.c
@@ -12,7 +12,30 @@
 
 #include "push_swap.h"
 
-void	ft_push()
+void	ft_pb(t_piles *pile)
 {
+	int	i;
+	int	j;
+
+	if (pile->len > 0 && pile->len_b < pile->len)
+	{
+		i = pile->len_b;
+		j = pile->len - 1 - pile->len_b;
+		(pile->b)[j] = (pile->a)[i];
+		pile->len_b++;
+	}
+}
+
+void	ft_pa(t_piles *pile)
+{
+	int	i;
+	int	j;
 	
+	if (pile->len > 0 && pile->len_b > 0)
+	{
+		i = pile->len_b - 1;
+		j = pile->len - pile->len_b;
+		(pile->a)[i] = (pile->b)[j];
+		pile->len_b--;
+	}
 }
