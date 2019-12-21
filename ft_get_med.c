d46825fc6c567ca7d1c1c17b5478fbd03ca3c31e
Daniel <dnl.coloma@gmail.com>
2019-12-21 21:32:21 +0100

    ADD get optimal pivot for quick sort

---
+++ ft_get_med.c
@@ -0,0 +1,19 @@
+#include "push_swap.h"
+
+void	ft_get_med(t_piles *pile)
+{
+	t_quicksort quick;
+	int			i;
+
+	quick = ft_sort(pile, QUICK_SORT);
+	pile->med = quick.tab[quick.len / 2];
+	i = 0;
+	while (pile->a[i] && pile->a[i] != pile->med)
+	{
+		i++;
+	}
+	pile->med_index = i;
+	free(quick.tab);
+	printf("med = %d\nindex = %d\n", pile->med, pile->med_index);
+	fflush(stdout);
+}
