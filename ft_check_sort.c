cb3ff7807fa60eea14c381d81dce17ab5261b992
Daniel <dnl.coloma@gmail.com>
2019-12-21 21:33:21 +0100

    UPDATE check if stack B is sorted

--- ft_check_sort.c
+++ ft_check_sort.c
@@ -1,15 +1,29 @@
 #include "push_swap.h"
 
-int		ft_check_sort(t_piles *pile)
+int		ft_check_sort(t_piles *pile, int desc)
 {
 	int	i;
 
-	i = pile->len_b + 1;
-	while (i < pile->len)
+	if (!desc)
 	{
-		if (pile->a[i - 1] > pile->a[i])
-			return (0);
-		i++;
+		i = pile->len_b + 1;
+		while (i < pile->len)
+		{
+			if (pile->a[i - 1] > pile->a[i])
+				return (0);
+			i++;
+		}
+		return (1);
 	}
-	return (1);
+	else
+	{
+		i = pile->len - pile->len_b + 1;
+		while (i < pile->len)
+		{
+			if (pile->b[i - 1] < pile->b[i])
+				return (0);
+			i++;
+		}
+		return (1);
+	}
 }
