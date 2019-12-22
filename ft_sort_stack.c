231be8f1146e666e916cc1979cd79d94ce8311e2
Daniel <dnl.coloma@gmail.com>
2019-12-22 23:33:37 +0100

    ADD sort left and right function quicksort

--- ft_sort_stack.c
+++ ft_sort_stack.c
@@ -15,29 +15,68 @@ int		ft_get_sorted_pos_B(t_piles *pile)
 	return (j);
 }
 
-void	ft_sort_stack(t_piles *pile)
+void 	t_get_best_rot(t_piles *pile, int pos)
 {
-	int	sorted_pos_b;
+	int	i;
+	int	limit;
 
-	ft_get_med(pile);
-	while (pile->a[pile->len_b] < pile->med)
+	i = 0;
+	if (pos > (pile->len_b) / 2)
 	{
-		sorted_pos_b = ft_get_sorted_pos_B(pile);
-		if (sorted_pos_b == 0)
+		limit = pile->len_b - pos;
+		while (i < limit)
 		{
-			ft_putstr("pb\n");
-			ft_pb(pile);
+			ft_putstr("rrb\n");
+			ft_rrb(pile);
+			i++;
 		}
-		else if (sorted_pos_b == 1)
+	}
+	else
+	{
+		while (i < pos)
 		{
 			ft_putstr("rb\n");
 			ft_rb(pile);
-			ft_putstr("pb\n");
-			ft_pb(pile);
+			i++;
 		}
-		else if (sorted_pos_b > 1)
+	}
+}
+
+void	ft_sort_left(t_piles *pile)
+{
+	int	sorted_pos_b;
+	
+	sorted_pos_b = ft_get_sorted_pos_B(pile);
+	if (sorted_pos_b < 1)
+	{
+		ft_putstr("pb\n");
+		ft_pb(pile);
+	}
+	else
+	{
+		// Rotate ou Reverse rotate B
+		ft_get_best_rot(pile, sorted_pos_b);
+		ft_putstr("pb\n");
+		ft_pb(pile);
+	}
+}
+
+void	ft_sort_right(t_piles *pile)
+{
+	
+}
+
+void	ft_sort_stack(t_piles *pile)
+{
+
+	ft_get_med(pile);
+	while (pile->len != 0 || !ft_check_sort(pile, 0))//(pile->a[pile->len_b] < pile->med)
+	{
+		if (pile->a[pile->len_b] < pile->med)
+			ft_sort_left(pile);
+		else
 		{
-			// Rotate ou Reverse rotate B 
+			ft_sort_right(pile);
 		}
 		
 		if (pile->len_b == 2 && !ft_check_sort(pile, 1))
