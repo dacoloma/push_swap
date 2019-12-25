6c76d880bc83e8cd986b9fd8ecf29fb805704cf9
Daniel <dnl.coloma@gmail.com>
2019-12-25 01:19:26 +0100

    UPDATE check sort

--- ft_check_sort.c
+++ ft_check_sort.c
@@ -3,26 +3,46 @@
 int		ft_check_sort(t_piles *pile, int desc)
 {
 	int	i;
+	int	j;
+	int	count;
 
 	if (!desc)
 	{
-		i = pile->len_b + 1;
-		while (i < pile->len)
+		// i = pile->len_b + 1;
+		pile->i_min = ft_get_min_index(pile->a, pile->len);
+		count = pile->len - pile->len_b - 1;
+		if (pile->len_b)
+			i = (pile->med_index + 1 < pile->len) ? pile->med_index + 1 : pile->len_b;
+		else
+			i = (pile->i_min + 1 < pile->len) ? pile->i_min + 1 : pile->len_b;
+		// while (i < pile->len)
+		// LEN_B;
+		// MED;
+		// printf("i_min = %d\n", pile->i_min);
+		// fflush(stdout);
+		while (count > 0)
 		{
-			if (pile->a[i - 1] > pile->a[i])
+			j = (i - 1 < pile->len_b) ? pile->len - 1 : i - 1;
+			if (pile->a[j] > pile->a[i])
 				return (0);
 			i++;
+			if (i == pile->len)
+				i = pile->len_b;
+			count--;
 		}
 		return (1);
 	}
 	else
 	{
-		i = pile->len - pile->len_b + 1;
-		while (i < pile->len)
+		if (pile->len_b)
 		{
-			if (pile->b[i - 1] < pile->b[i])
-				return (0);
-			i++;
+			i = pile->len - pile->len_b + 1;
+			while (i < pile->len)
+			{
+				if (pile->b[i - 1] < pile->b[i])
+					return (0);
+				i++;
+			}
 		}
 		return (1);
 	}
