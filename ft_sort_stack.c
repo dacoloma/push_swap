619d39813251d9662bddcc8d53a1bec03bda7bb0
Daniel <dnl.coloma@gmail.com>
2019-12-25 11:53:51 +0100

    UPDATE reset sort right fct

--- ft_sort_stack.c
+++ ft_sort_stack.c
@@ -129,58 +129,7 @@ void	ft_sort_right(t_piles *pile)
 	int	sorted_pos_a;
 
 	sorted_pos_a = ft_get_sorted_pos_A(pile);
-	// ft_print_piles(pile);
-	// POS_A;
-	if (sorted_pos_a == pile->len_b)
-	{
-		ft_putstr("ra\n");
-		ft_ra(pile);
-	}
-	else if (sorted_pos_a == pile->len_b + 1)
-	{
-		ft_putstr("sa\n");
-		ft_sa(pile);
-		if (!ft_check_sort(pile, 0))
-		{
-			ft_putstr("ra\n");
-			ft_ra(pile);
-			ft_putstr("ra\n");
-			ft_ra(pile);
-		}
-	}
 	
-	else if (sorted_pos_a > pile->len_b + 2 && sorted_pos_a < pile->len - 1)
-	{
-		ft_putstr("pb\n");
-		ft_pb(pile);
-		ft_get_best_rot_A(pile, sorted_pos_a - 1);
-		ft_putstr("pa\n");
-		ft_pa(pile);
-	}
-	else if (sorted_pos_a == pile->len - 1)
-	{
-		ft_putstr("rra\n");
-		ft_rra(pile);
-		ft_putstr("sa\n");
-		ft_sa(pile);
-		// DEBUG;
-		if (!ft_check_sort(pile, 0))
-		{
-			ft_putstr("ra\n");
-			ft_ra(pile);
-			ft_putstr("ra\n");
-			ft_ra(pile);
-		}
-	}
-	else if (sorted_pos_a == pile->len_b + 2)
-	{
-		ft_putstr("sa\n");
-		ft_sa(pile);
-		ft_putstr("ra\n");
-		ft_ra(pile);
-		ft_putstr("ra\n");
-		ft_ra(pile);
-	}
 }
 
 void	ft_sort_stack(t_piles *pile)
