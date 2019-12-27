882dc11c0bce0bf54450e4648e65008f094ec7f1
Daniel <dnl.coloma@gmail.com>
2019-12-27 16:15:11 +0100

    EDIT remove comment

--- ft_check_sort.c
+++ ft_check_sort.c
@@ -8,18 +8,12 @@ int		ft_check_sort(t_piles *pile, int desc)
 
 	if (!desc)
 	{
-		// i = pile->len_b + 1;
 		pile->min_index = ft_get_min_index(pile->a, pile->len);
 		count = pile->len - pile->len_b - 1;
 		if (pile->len_b)
 			i = (pile->med_index + 1 < pile->len) ? pile->med_index + 1 : pile->len_b;
 		else
 			i = (pile->min_index + 1 < pile->len) ? pile->min_index + 1 : pile->len_b;
-		// while (i < pile->len)
-		// LEN_B;
-		// MED;
-		// printf("min_index = %d\n", pile->min_index);
-		// fflush(stdout);
 		while (count > 0)
 		{
 			j = (i - 1 < pile->len_b) ? pile->len - 1 : i - 1;
