675f1c74dfb3a97c3de840fd47d9ec54e06ae427
Daniel <dnl.coloma@gmail.com>
2019-12-30 20:45:22 +0100

    NORME

--- push_swap.c
+++ push_swap.c
@@ -12,34 +12,6 @@
 
 #include "push_swap.h"
 
-// static void	ft_pb_min(t_piles *pile)
-// {
-// 	int	i;
-
-// 	if (pile->i_min <= 1)
-// 		return ;
-// 	i = 0;
-// 	if (pile->i_min <= pile->len - pile->i_min)
-// 	{
-// 		while (i < pile->i_min)
-// 		{
-// 			ft_putstr("ra\n");
-// 			ft_ra(pile);
-// 			i++;
-// 		}
-// 	}
-// 	else
-// 	{
-// 		while (i < pile->len - pile->i_min)
-// 		{
-// 			ft_putstr("rra\n");
-// 			ft_rra(pile);
-// 			i++;
-// 		}
-// 	}
-// 	ft_pb(pile);
-// }
-
 int	main(int ac, char **av)
 {
 	char	**args;
@@ -56,17 +28,9 @@ int	main(int ac, char **av)
 		ft_putstr_err("Error\n");
 		exit(-1);
 	}
-	// ft_print_char_tab(args);
 	if (!ft_init(&pile, args))
-	{
 		exit(-1);
-		DEBUG;
-	}
-	// ft_print_piles(pile);
-	// ft_print_int_tab(pile->a, pile->len);
-	// ft_sort(pile, QUICK_SORT);
 	ft_sort_stack(pile);
-	// ft_print_piles(pile);
 	free(pile->a);
 	free(pile->b);
 	free(pile);
