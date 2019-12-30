81e1ae44c1bc2e5be58d02d67e188d1015f7d563
Daniel <dnl.coloma@gmail.com>
2019-12-30 20:47:01 +0100

    ADD recursive sort

--- ft_sort_stack.c
+++ ft_sort_stack.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-void	ft_short_stack(t_piles *pile)
+static void	ft_short_stack(t_piles *pile)
 {
 	while (!ft_check_sort(pile, ASC))
 	{
@@ -10,82 +10,49 @@ void	ft_short_stack(t_piles *pile)
 	ft_get_best_rot_A(pile, pile->min_index);
 }
 
-void	ft_sort_left(t_piles *pile)
+static void	ft_left_right_sort(t_piles *pile)
 {
-	int	sorted_pos_b;
-	
-	sorted_pos_b = ft_get_sorted_pos_B(pile);
-	if (sorted_pos_b < 1)
+	if (!ft_check_sort(pile, 0))
 	{
-		ft_putstr("pb\n");
-		ft_pb(pile);
-	}
-	else
-	{
-		ft_get_best_rot_B(pile, sorted_pos_b);
-		ft_putstr("pb\n");
-		ft_pb(pile);
-	}
-}
-
-void	ft_sort_right(t_piles *pile)
-{
-	int	sorted_pos_a;
-
-	sorted_pos_a = ft_get_sorted_pos_A(pile);
-	// POS_A;
-	if (sorted_pos_a == pile->len_b + 2)
-	{
-		ft_putstr("sa\n");
-		ft_sa(pile);
-		if (!ft_check_sort(pile, 0))
+		if (pile->a[pile->len_b] < pile->med)
+		{
+			ft_putstr("pb\n");
+			ft_pb(pile);
+		}
+		else
 		{
 			ft_putstr("ra\n");
 			ft_ra(pile);
-			// ft_putstr("ra\n");
-			// ft_ra(pile);
 		}
 	}
-	else if (pile->len_b == pile->med_index || sorted_pos_a == pile->len_b)
-	{
-		ft_putstr("ra\n");
-		ft_ra(pile);
-	}
-	else if (sorted_pos_a == pile->len - 1)
-	{
-		ft_putstr("rra\n");
-		ft_rra(pile);
-		ft_putstr("sa\n");
-		ft_sa(pile);
-		// DEBUG;
-		if (!ft_check_sort(pile, 0))
-		{
-			ft_putstr("ra\n");
-			ft_ra(pile);
-			// ft_putstr("ra\n");
-			// ft_ra(pile);
-		}
-	}
-	else
-	{
-		ft_putstr("pb\n");
-		ft_pb(pile);
-		ft_get_best_rot_A(pile, sorted_pos_a);
-		ft_putstr("pa\n");
-		ft_pa(pile);
-		ft_putstr("ra\n");
-		ft_ra(pile);
-	}
-	
 }
 
-void	ft_sort_stack(t_piles *pile)
+void		ft_sort_stack(t_piles *pile)
 {
-	
-	if (pile->len <= 3)
-	{
-		ft_short_stack(pile);
+	if (pile->min_index == 0 && ft_check_sort(pile, 0))
 		return ;
+	if (pile->len - pile->len_b <= 3)
+	{
+		if (!pile->len_b || (pile->len_b
+			&& pile->a[pile->min_index] > pile->b[pile->max_index_b]))
+			ft_short_stack(pile);
 	}
-	
+	if  (ft_check_sort(pile, 0))
+	{
+		//REARRANGE STACK A
+		if (!pile->len_b
+			|| (pile->len_b && pile->b[pile->max_index_b] < pile->a[pile->min_index]))
+			ft_get_best_rot_A(pile, pile->min_index);
+		else
+			ft_get_best_rot_A(pile, pile->med_index);
+		//REARRANGE STACK B
+		ft_get_best_rot_B(pile, pile->max_index_b);
+		while (pile->len_b)
+		{
+			ft_putstr("pa\n");
+			ft_pa(pile);
+		}
+	}
+	ft_left_right_sort(pile);
+	ft_sort_stack(pile);
 }
