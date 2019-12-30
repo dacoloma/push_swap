0dd51cb05bac53ee15d71aaa1f438eec69952f93
Daniel <dnl.coloma@gmail.com>
2019-12-30 20:43:13 +0100

    UPDATE test.sh

--- test.sh
+++ test.sh
@@ -5,203 +5,225 @@ NOCOLOR="\033[0m"
 
 clear
 
-# testing3=(
-# 	"1 2 3"
-# 	"1 3 2"
-# 	"2 1 3"
-# 	"2 3 1"
-# 	"3 1 2"
-# 	"3 2 1"
-# )
-
-# for (( i=0; i < ${#testing3[@]}; i++ )); 
-# do
-# printf "\n#-----------------#\n#test %d => ${testing3[i]}: " $i
-# ./push_swap ${testing3[i]} > tmp
-# test_pushswap=$(./push_swap ${testing3[i]} | ./checker ${testing3[i]})
-# if [ "$test_pushswap" = "OK" ] ; then
-# 	echo "${GREEN}${test_pushswap}${NOCOLOR}
-# nb_instructions=$(cat tmp | wc -l)
-
-# elif [ "$test_pushswap" = "KO" ] ; then
-# 	echo "${RED}${test_pushswap}${NOCOLOR}"
-# fi
-# cat tmp | tr '\n' ' ' | cat
-# done
-
-# testing4=(
-# 	"1 2 3 4"
-# 	"1 2 4 3"
-# 	"1 3 2 4"
-# 	"1 3 4 2"
-# 	"1 4 2 3"
-# 	"1 4 3 2"
-# 	"2 1 3 4"
-# 	"2 1 4 3"
-# 	"2 3 1 4"
-# 	"2 3 4 1"
-# 	"2 4 1 3"
-# 	"2 4 3 1"
-# 	"3 1 2 4"
-# 	"3 1 4 2"
-# 	"3 2 1 4"
-# 	"3 2 4 1"
-# 	"3 4 1 2"
-# 	"3 4 2 1"
-# 	"4 1 2 3"
-# 	"4 1 3 2"
-# 	"4 2 1 3"
-# 	"4 2 3 1"
-# 	"4 3 1 2"
-# 	"4 3 2 1"
-# )
-
-# for (( i=0; i < ${#testing4[@]}; i++ )); 
-# do 
-# printf "\n#-----------------#\n#test %d => ${testing4[i]}: " $i
-# ./push_swap ${testing4[i]} > tmp
-# test_pushswap=$(cat tmp | ./checker ${testing4[i]})
-# if [ "$test_pushswap" = "OK" ] ; then
-# 	printf "${GREEN}${test_pushswap}${NOCOLOR}"
-# 	nb_instructions=$(wc -l < tmp)
-# 	# echo "TEST : $nb_instructions"
-# 	if (( "$nb_instructions" < 5 )) ; then
-# 		echo "${GREEN}${nb_instructions}${NOCOLOR}"
-# 	elif (( "$nb_instructions" < 12 )) ; then
-# 		echo "${ORANGE}${nb_instructions}${NOCOLOR}"
-# 	else
-# 		echo "${RED}${nb_instructions}${NOCOLOR}"
-# 	fi
-# elif [ "$test_pushswap" = "KO" ] ; then
-# 	echo "${RED}${test_pushswap}${NOCOLOR}"
-# fi
-# cat tmp | tr '\n' ' ' | cat
-# done
-
+testing3=(
+	"1 2 3"
+	"1 3 2"
+	"2 1 3"
+	"2 3 1"
+	"3 1 2"
+	"3 2 1"
+)
+testing4=(
+	"1 2 3 4"
+	"1 2 4 3"
+	"1 3 2 4"
+	"1 3 4 2"
+	"1 4 2 3"
+	"1 4 3 2"
+	"2 1 3 4"
+	"2 1 4 3"
+	"2 3 1 4"
+	"2 3 4 1"
+	"2 4 1 3"
+	"2 4 3 1"
+	"3 1 2 4"
+	"3 1 4 2"
+	"3 2 1 4"
+	"3 2 4 1"
+	"3 4 1 2"
+	"3 4 2 1"
+	"4 1 2 3"
+	"4 1 3 2"
+	"4 2 1 3"
+	"4 2 3 1"
+	"4 3 1 2"
+	"4 3 2 1"
+)
 testing5=(
-	"1 2 3 4 5" 
-	"2 1 3 4 5" 
-	"3 1 2 4 5" 
-	"1 3 2 4 5" 
-	"2 3 1 4 5" 
-	"3 2 1 4 5" 
-	"4 2 3 1 5" 
-	"2 4 3 1 5" 
-	"3 4 2 1 5" 
-	"4 3 2 1 5" 
-	"2 3 4 1 5" 
-	"3 2 4 1 5" 
-	"4 1 3 2 5" 
-	"1 4 3 2 5" 
-	"3 4 1 2 5" 
-	"4 3 1 2 5" 
-	"1 3 4 2 5" 
-	"3 1 4 2 5" 
-	"4 1 2 3 5" 
-	"1 4 2 3 5" 
-	"2 4 1 3 5" 
-	"4 2 1 3 5" 
-	"1 2 4 3 5" 
-	"2 1 4 3 5" 
-	"5 1 2 3 4" 
-	"1 5 2 3 4" 
-	"2 5 1 3 4" 
-	"5 2 1 3 4" 
-	"1 2 5 3 4" 
-	"2 1 5 3 4" 
-	"3 1 2 5 4" 
-	"1 3 2 5 4" 
-	"2 3 1 5 4" 
-	"3 2 1 5 4" 
-	"1 2 3 5 4" 
-	"2 1 3 5 4" 
-	"3 5 2 1 4" 
-	"5 3 2 1 4" 
-	"2 3 5 1 4" 
-	"3 2 5 1 4" 
-	"5 2 3 1 4" 
-	"2 5 3 1 4" 
-	"3 5 1 2 4" 
-	"5 3 1 2 4" 
-	"1 3 5 2 4" 
-	"3 1 5 2 4" 
-	"5 1 3 2 4" 
-	"1 5 3 2 4" 
-	"4 5 1 2 3" 
-	"5 4 1 2 3" 
-	"1 4 5 2 3" 
-	"4 1 5 2 3" 
-	"5 1 4 2 3" 
-	"1 5 4 2 3" 
-	"2 5 1 4 3" 
-	"5 2 1 4 3" 
-	"1 2 5 4 3" 
-	"2 1 5 4 3" 
-	"5 1 2 4 3" 
-	"1 5 2 4 3" 
-	"2 4 1 5 3" 
-	"4 2 1 5 3" 
-	"1 2 4 5 3" 
-	"2 1 4 5 3" 
-	"4 1 2 5 3" 
-	"1 4 2 5 3" 
-	"2 4 5 1 3" 
-	"4 2 5 1 3" 
-	"5 2 4 1 3" 
-	"2 5 4 1 3" 
-	"4 5 2 1 3" 
-	"5 4 2 1 3" 
-	"3 4 5 1 2" 
-	"4 3 5 1 2" 
-	"5 3 4 1 2" 
-	"3 5 4 1 2" 
-	"4 5 3 1 2" 
-	"5 4 3 1 2" 
-	"1 4 5 3 2" 
-	"4 1 5 3 2" 
-	"5 1 4 3 2" 
-	"1 5 4 3 2" 
-	"4 5 1 3 2" 
-	"5 4 1 3 2" 
-	"1 3 5 4 2" 
-	"3 1 5 4 2" 
-	"5 1 3 4 2" 
-	"1 5 3 4 2" 
-	"3 5 1 4 2" 
-	"5 3 1 4 2" 
-	"1 3 4 5 2" 
-	"3 1 4 5 2" 
-	"4 1 3 5 2" 
-	"1 4 3 5 2" 
-	"3 4 1 5 2" 
-	"4 3 1 5 2" 
-	"2 3 4 5 1" 
-	"3 2 4 5 1" 
-	"4 2 3 5 1" 
-	"2 4 3 5 1" 
-	"3 4 2 5 1" 
-	"4 3 2 5 1" 
-	"5 3 4 2 1" 
-	"3 5 4 2 1" 
-	"4 5 3 2 1" 
-	"5 4 3 2 1" 
-	"3 4 5 2 1" 
-	"4 3 5 2 1" 
-	"5 2 4 3 1" 
-	"2 5 4 3 1" 
-	"4 5 2 3 1" 
-	"5 4 2 3 1" 
-	"2 4 5 3 1" 
-	"4 2 5 3 1" 
-	"5 2 3 4 1" 
-	"2 5 3 4 1" 
-	"3 5 2 4 1" 
-	"5 3 2 4 1" 
-	"2 3 5 4 1" 
-	"3 2 5 4 1" 
-
+	"1 2 3 4 5"
+	"2 1 3 4 5"
+	"3 1 2 4 5"
+	"1 3 2 4 5"
+	"2 3 1 4 5"
+	"3 2 1 4 5"
+	"4 2 3 1 5"
+	"2 4 3 1 5"
+	"3 4 2 1 5"
+	"4 3 2 1 5"
+	"2 3 4 1 5"
+	"3 2 4 1 5"
+	"4 1 3 2 5"
+	"1 4 3 2 5"
+	"3 4 1 2 5"
+	"4 3 1 2 5"
+	"1 3 4 2 5"
+	"3 1 4 2 5"
+	"4 1 2 3 5"
+	"1 4 2 3 5"
+	"2 4 1 3 5"
+	"4 2 1 3 5"
+	"1 2 4 3 5"
+	"2 1 4 3 5"
+	"5 1 2 3 4"
+	"1 5 2 3 4"
+	"2 5 1 3 4"
+	"5 2 1 3 4"
+	"1 2 5 3 4"
+	"2 1 5 3 4"
+	"3 1 2 5 4"
+	"1 3 2 5 4"
+	"2 3 1 5 4"
+	"3 2 1 5 4"
+	"1 2 3 5 4"
+	"2 1 3 5 4"
+	"3 5 2 1 4"
+	"5 3 2 1 4"
+	"2 3 5 1 4"
+	"3 2 5 1 4"
+	"5 2 3 1 4"
+	"2 5 3 1 4"
+	"3 5 1 2 4"
+	"5 3 1 2 4"
+	"1 3 5 2 4"
+	"3 1 5 2 4"
+	"5 1 3 2 4"
+	"1 5 3 2 4"
+	"4 5 1 2 3"
+	"5 4 1 2 3"
+	"1 4 5 2 3"
+	"4 1 5 2 3"
+	"5 1 4 2 3"
+	"1 5 4 2 3"
+	"2 5 1 4 3"
+	"5 2 1 4 3"
+	"1 2 5 4 3"
+	"2 1 5 4 3"
+	"5 1 2 4 3"
+	"1 5 2 4 3"
+	"2 4 1 5 3"
+	"4 2 1 5 3"
+	"1 2 4 5 3"
+	"2 1 4 5 3"
+	"4 1 2 5 3"
+	"1 4 2 5 3"
+	"2 4 5 1 3"
+	"4 2 5 1 3"
+	"5 2 4 1 3"
+	"2 5 4 1 3"
+	"4 5 2 1 3"
+	"5 4 2 1 3"
+	"3 4 5 1 2"
+	"4 3 5 1 2"
+	"5 3 4 1 2"
+	"3 5 4 1 2"
+	"4 5 3 1 2"
+	"5 4 3 1 2"
+	"1 4 5 3 2"
+	"4 1 5 3 2"
+	"5 1 4 3 2"
+	"1 5 4 3 2"
+	"4 5 1 3 2"
+	"5 4 1 3 2"
+	"1 3 5 4 2"
+	"3 1 5 4 2"
+	"5 1 3 4 2"
+	"1 5 3 4 2"
+	"3 5 1 4 2"
+	"5 3 1 4 2"
+	"1 3 4 5 2"
+	"3 1 4 5 2"
+	"4 1 3 5 2"
+	"1 4 3 5 2"
+	"3 4 1 5 2"
+	"4 3 1 5 2"
+	"2 3 4 5 1"
+	"3 2 4 5 1"
+	"4 2 3 5 1"
+	"2 4 3 5 1"
+	"3 4 2 5 1"
+	"4 3 2 5 1"
+	"5 3 4 2 1"
+	"3 5 4 2 1"
+	"4 5 3 2 1"
+	"5 4 3 2 1"
+	"3 4 5 2 1"
+	"4 3 5 2 1"
+	"5 2 4 3 1"
+	"2 5 4 3 1"
+	"4 5 2 3 1"
+	"5 4 2 3 1"
+	"2 4 5 3 1"
+	"4 2 5 3 1"
+	"5 2 3 4 1"
+	"2 5 3 4 1"
+	"3 5 2 4 1"
+	"5 3 2 4 1"
+	"2 3 5 4 1"
+	"3 2 5 4 1"
 )
 
+# 3 NUMBERS
+
+for (( i=0; i < ${#testing3[@]}; i++ )); 
+do
+printf "\n#-----------------#\n#test %d => ${testing3[i]}: " $i
+./push_swap ${testing3[i]} > tmp
+test_pushswap=$(./push_swap ${testing3[i]} | ./checker ${testing3[i]})
+if [ "$test_pushswap" = "OK" ] ; then
+	printf "${GREEN}${test_pushswap}${NOCOLOR}"
+	nb_instructions=$(wc -l < tmp)
+	if (( "$nb_instructions" < 3 )) ; then
+		echo "${GREEN}${nb_instructions}${NOCOLOR}"
+	elif (( "$nb_instructions" < 5 )) ; then
+		echo "${ORANGE}${nb_instructions}${NOCOLOR}"
+	else
+		echo "${RED}${nb_instructions}${NOCOLOR}"
+	fi
+elif [ "$test_pushswap" = "KO" ] ; then
+	echo "${RED}${test_pushswap}${NOCOLOR}"
+fi
+cat tmp | tr '\n' ' ' | cat
+done
+
+read -n 1 -s -p "
+Press any key to continue TEST 4 NUMBERS ?(y/n)" var
+if [ "$var" = "n" ] ; then
+	rm tmp
+	exit
+fi
+printf "===================================\n"
+
+# 4 NUMBERS
+for (( i=0; i < ${#testing4[@]}; i++ )); 
+do 
+printf "\n#-----------------#\n#test %d => ${testing4[i]}: " $i
+./push_swap ${testing4[i]} > tmp
+test_pushswap=$(cat tmp | ./checker ${testing4[i]})
+if [ "$test_pushswap" = "OK" ] ; then
+	printf "${GREEN}${test_pushswap}${NOCOLOR}"
+	nb_instructions=$(wc -l < tmp)
+	if (( "$nb_instructions" < 5 )) ; then
+		echo "${GREEN}${nb_instructions}${NOCOLOR}"
+	elif (( "$nb_instructions" < 8 )) ; then
+		echo "${ORANGE}${nb_instructions}${NOCOLOR}"
+	else
+		echo "${RED}${nb_instructions}${NOCOLOR}"
+	fi
+elif [ "$test_pushswap" = "KO" ] ; then
+	echo "${RED}${test_pushswap}${NOCOLOR}"
+fi
+cat tmp | tr '\n' ' ' | cat
+done
+
+read -n 1 -s -p "
+Press any key to continue TEST 5 NUMBERS ?(y/n)" var
+if [ "$var" = "n" ]; then
+	rm tmp
+	exit
+fi
+printf "===================================\n"
+
+# 5 NUMBERS
 for (( i=0; i < ${#testing5[@]}; i++ )); 
 do 
 if ((i == 20)) || ((i == 40)) || ((i == 60)) || ((i == 80)) || ((i == 100)); then
@@ -215,7 +237,6 @@ test_pushswap=$(cat tmp | ./checker ${testing5[i]})
 if [ "$test_pushswap" = "OK" ] ; then
 	printf "${GREEN}${test_pushswap}${NOCOLOR}"
 	nb_instructions=$(wc -l < tmp)
-	# echo "TEST : $nb_instructions"
 	if (( "$nb_instructions" < 5 )) ; then
 		echo "${GREEN}${nb_instructions}${NOCOLOR}"
 	elif (( "$nb_instructions" < 12 )) ; then
