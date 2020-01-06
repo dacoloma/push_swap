5437dda3e5ffa07e720cf32aea1c886a482dc9cf
Daniel <dnl.coloma@gmail.com>
2020-01-06 17:55:26 +0100

    UPDATE sort program

--- ft_sort_stack.c
+++ ft_sort_stack.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-static void	ft_short_stack(t_piles *pile)
+void	ft_short_stack(t_piles *pile)
 {
 	while (!ft_check_sort(pile))
 	{
@@ -40,55 +40,14 @@ void	ft_sort_left(t_piles *pile)
 	}
 }
 
-static void	ft_left_right_sort(t_piles *pile)
-{
-	if (!ft_check_sort(pile))
-	{
-		if (pile->a[pile->len_b] < pile->med)
-		{
-			ft_sort_left(pile);
-		}
-		else
-		{
-			ft_putstr("ra\n");
-			ft_ra(pile);
-		}
-	}
-}
-
 void		ft_sort_stack(t_piles *pile)
 {
-	// MED;
-	if (pile->min_index == 0 && ft_check_sort(pile))
+	if (ft_check_sort(pile) && pile->min_index == 0)
 		return ;
-	if (ft_check_split(pile))
+	if (pile->len < 7)
+		ft_medium_stack(pile);
+	else
 	{
-		ft_sort_stack_a(pile);
+		ft_long_stack(pile);
 	}
-	if (pile->len - pile->len_b <= 3)
-	{
-		if (!pile->len_b || (pile->len_b
-			&& pile->a[pile->min_index] > pile->b[pile->max_index_b]))
-			ft_short_stack(pile);
-	}
-	if  (ft_check_sort(pile))
-	{
-		//REARRANGE STACK A
-		if (!pile->len_b
-			|| (pile->len_b && pile->b[pile->max_index_b] < pile->a[pile->min_index]))
-			ft_get_best_rot_A(pile, pile->min_index);
-		else
-			ft_get_best_rot_A(pile, pile->med_index);
-		//REARRANGE STACK B
-		ft_get_best_rot_B(pile, pile->max_index_b);
-		while (pile->len_b)
-		{
-			ft_putstr("pa\n");
-			ft_pa(pile);
-		}
-	}
-	ft_left_right_sort(pile);
-	// DEBUG;
-	// sleep(1);
-	ft_sort_stack(pile);
 }
