a06761fbebc5003c3a38d3f3a79c0621d469dbef
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2020-01-08 14:05:02 +0100

    UPDATE backup2

--- backup2.c
+++ backup2.c
@@ -12,62 +12,106 @@
 
 #include "push_swap.h"
 
-static void	ft_short_stack(t_piles *pile)
+static int	ft_count(t_piles *pile)
 {
-	while (!ft_check_sort(pile))
+	int	i;
+	int count;
+
+	count = 0;
+	i = pile->len_b;
+	while (i < pile->len)
 	{
-		ft_putstr("sa\n");
-		ft_sa(pile);
+		if (pile->a[i] < pile->med)
+			count++;
+		i++;
 	}
-	ft_get_best_rot_A(pile, pile->min_index);
+	return (count);
 }
 
-static void	ft_left_right_sort(t_piles *pile)
+// static void	ft_sub_partition(t_piles *pile)
+// {
+// 	int	sub_med;
+// 	int	sub_med_index;
+// 	int	min;
+
+// 	min = pile->a[pile->min_index];
+// 	sub_med_index = pile->len / 2 + ft_get_index(pile->quick.tab, min) / 2;
+// 	sub_med = pile->quick.tab[sub_med_index];
+// 	MED;
+// 	SUB_MED;
+// 	if (pile->len_b > 1)
+// 		if (pile->b[pile->len - pile->len_b] < sub_med)
+// 		{
+// 			ft_putstr("rb\n");
+// 			ft_rb(pile);
+// 		}
+// }
+
+int			ft_partition(t_piles *pile)
 {
-	if (!ft_check_sort(pile))
+	int	count;
+
+	if (pile->len - pile->len_b <= 3)// && ft_check_split(pile))
+		return (1);
+	ft_get_med(pile);
+	ft_get_min_index(pile);
+	// MIN_A;
+	count = ft_count(pile);
+	while (count > 0)
 	{
 		if (pile->a[pile->len_b] < pile->med)
 		{
 			ft_putstr("pb\n");
 			ft_pb(pile);
+			// ft_sub_partition(pile);
+			count--;
 		}
 		else
 		{
 			ft_putstr("ra\n");
 			ft_ra(pile);
 		}
+		// DEBUG;
+		// sleep(1);
 	}
+	// MED;
+	// sleep(1);
+	ft_partition(pile);
+	return (0);
 }
 
-void		ft_sort_stack(t_piles *pile)
+static int			ft_get_pos(t_piles *pile)
 {
-	// MED;
-	if (pile->min_index == 0 && ft_check_sort(pile))
-		return ;
-	if (pile->len - pile->len_b <= 3)
+	int	i;
+	int	j;
+
+	ft_get_min_index(pile);
+	i = pile->min_index;
+	j = pile->len - pile->len_b;
+	while (pile->a[i] < pile->b[j])
 	{
-		if (!pile->len_b || (pile->len_b
-			&& pile->a[pile->min_index] > pile->b[pile->max_index_b]))
-			ft_short_stack(pile);
+		i++;
+		if (i == pile->len)
+			i = pile->len_b;
 	}
-	if  (ft_check_sort(pile))
+	return (i);
+}
+
+
+
+void		ft_long_stack(t_piles *pile)
+{
+	ft_partition(pile);
+	ft_short_stack(pile);
+	// DEBUG;
+	int	sorted_pos_a;
+	while (pile->len_b)
 	{
-		//REARRANGE STACK A
-		if (!pile->len_b
-			|| (pile->len_b && pile->b[pile->max_index_b] < pile->a[pile->min_index]))
-			ft_get_best_rot_A(pile, pile->min_index);
-		else
-			ft_get_best_rot_A(pile, pile->med_index);
-		//REARRANGE STACK B
-		ft_get_best_rot_B(pile, pile->max_index_b);
-		while (pile->len_b)
-		{
-			ft_putstr("pa\n");
-			ft_pa(pile);
-		}
+		sorted_pos_a = ft_get_pos(pile);
+		ft_get_best_rot_A(pile, sorted_pos_a);
+		ft_putstr("pa\n");
+		ft_pa(pile);
 	}
-	ft_left_right_sort(pile);
-	DEBUG;
-	sleep(1);
-	ft_sort_stack(pile);
+	ft_get_min_index(pile);
+	ft_get_best_rot_A(pile, pile->min_index);
 }
