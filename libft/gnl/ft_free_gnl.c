aeeea1fb621b721a5196512c9eb1df661d27a9c7
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-01-20 19:32:11 +0100

    ADD ft_free_gnl

--- libft/gnl/ft_free_gnl.c
+++ libft/gnl/ft_free_gnl.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl.h                                              :+:      :+:    :+:   */
+/*   ft_free_gnl.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcoloma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/14 17:10:03 by dcoloma           #+#    #+#             */
-/*   Updated: 2020/01/14 17:10:11 by dcoloma          ###   ########.fr       */
+/*   Created: 2020/01/20 18:50:39 by dcoloma           #+#    #+#             */
+/*   Updated: 2020/01/20 18:50:43 by dcoloma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GNL_H
-# define GNL_H
-# include "libft.h"
-# define GNL_ERROR -1
-# define GNL_READ 1
-# define GNL_EOF 0
+#include "gnl.h"
 
-typedef struct	s_gnl
+void	ft_free_gnl(t_gnl *elem)
 {
-	char			*content;
-	int				fd;
-	struct s_gnl	*next;
-}				t_gnl;
-t_gnl			*ft_new_elem(char *content, int fd);
-void			ft_add_elem(t_gnl **alst, t_gnl *new);
-t_gnl			*ft_get_elem(t_gnl **head, const int fd);
-void			ft_free_gnl(t_gnl *elem);
-#endif
+	ft_strdel(&elem->content);
+	free(elem);
+}
