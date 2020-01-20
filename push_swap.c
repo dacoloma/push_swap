4348ffd732d6e44e25a96238f7a604f9f7ea7e88
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-01-20 13:36:15 +0100

    UPDATE improve readability

--- push_swap.c
+++ push_swap.c
@@ -14,16 +14,14 @@
 
 static int	ft_get_arg(int ac, char **av, char ***args)
 {
-	int		ret;
 	int		i;
 	char	**tmp1;
 	char	**tmp2;
 
-	ret = 0;
 	if (ac == 1)
 		exit(0);
 	if (ac < 2)
-		return (-1);
+		return (INVALID);
 	*args = ft_strsplit(av[1], ' ');
 	i = 2;
 	while (i < ac && av[i])
@@ -35,18 +33,18 @@ static int	ft_get_arg(int ac, char **av, char ***args)
 		ft_free_tab(tmp2);
 		i++;
 	}
-	return (ret);
+	return (VALID);
 }
 
 int			main(int ac, char **av)
 {
 	char	**args;
 	t_piles	*pile;
-	int		ret;
 	int		flag;
 
-	ret = ft_get_arg(ac, av, &args);
-	if (ret == -1 || ft_is_valid(args, &flag) == 0 || !ft_init(&pile, args, 0))
+	if (ft_get_arg(ac, av, &args) == INVALID
+		|| ft_is_valid(args, &flag) == INVALID
+			|| ft_init(&pile, args, 0) == INVALID)
 	{
 		ft_putstr_err("Error\n");
 		exit(-1);
