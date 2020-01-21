c4535187e31bde8c25770c13e10ef8ad52cd49e0
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-01-21 19:57:28 +0100

    FIX segfault

--- ft_get_args.c
+++ ft_get_args.c
@@ -12,7 +12,16 @@
 
 #include "push_swap.h"
 
-int	ft_get_args(int ac, char **av, char ***args)
+static int	ft_check_null_pointer(char **args)
+{
+	if (*args == NULL)
+	{
+		return (INVALID);
+	}
+	return (VALID);
+}
+
+int			ft_get_args(int ac, char **av, char ***args)
 {
 	int		i;
 	char	**tmp1;
@@ -23,6 +32,8 @@ int	ft_get_args(int ac, char **av, char ***args)
 	if (ac < 2)
 		return (INVALID);
 	*args = ft_strsplit(av[1], ' ');
+	if (ft_check_null_pointer(*args) == INVALID)
+		return (INVALID);
 	i = 2;
 	while (i < ac && av[i])
 	{
@@ -31,11 +42,8 @@ int	ft_get_args(int ac, char **av, char ***args)
 		*args = ft_tabjoin(tmp1, tmp2);
 		ft_free_tab(tmp1);
 		ft_free_tab(tmp2);
-		if (*args == NULL)
-		{
-			ft_putstr_err("Cannot allocate enough memory.\n");
+		if (ft_check_null_pointer(*args) == INVALID)
 			return (INVALID);
-		}
 		i++;
 	}
 	return (VALID);
