c0af102968b74f9114444edb68bde9dea01f0fe8
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-01-21 14:38:38 +0100

    UPDATE test script

--- test.sh
+++ test.sh
@@ -13,23 +13,72 @@ Enter MAX value: " MAX
 read -p "
 Enter RANGE value: " RANGE
 
-
-entries="$(python3 rand_nb_gen.py $MIN $MAX $RANGE)"
-printf "\n#-----------------#\n#test %d => $entries:\t" $i
-./push_swap $entries > tmp
-test_pushswap=$(cat tmp | ./checker ${entries})
-if [ "$test_pushswap" = "OK" ] ; then
-	printf "${GREEN}${test_pushswap}${NOCOLOR}"
-	nb_instructions=$(wc -l < tmp)
-	if (( "$nb_instructions" < 5 )) ; then
-		echo "${GREEN}${nb_instructions}${NOCOLOR}"
-	elif (( "$nb_instructions" < 12 )) ; then
-		echo "${ORANGE}${nb_instructions}${NOCOLOR}"
-	else
-		echo "${RED}${nb_instructions}${NOCOLOR}"
+limit3=(
+	"3"
+	"4"
+)
+limit5=(
+	"5"
+	"12"
+)
+limit100=(
+	"700"
+	"750"
+)
+limit500=(
+	"5300"
+	"5400"
+)
+limit=(
+	limit3
+	limit5
+	limit100
+	limit500
+)
+i=0
+while true
+do
+	entries="$(python3 rand_nb_gen.py $MIN $MAX $RANGE)"
+	printf "\n#-----------------#\n#test %d => $entries:\t" $i
+	i=$(($i+1))
+	if (( "$RANGE" < 4)) ; then
+		j=0
+	elif (( "$RANGE" < 6)) ; then
+		j=1
+	elif (( "$RANGE" < 101)) ; then
+		j=2
+	elif (( "$RANGE" < 501)) ; then
+		j=3
 	fi
-elif [ "$test_pushswap" = "KO" ] ; then
-	echo "${RED}${test_pushswap}${NOCOLOR}"
-fi
-rm -f tmp
+	./push_swap $entries > tmp
+	test_pushswap=$(cat tmp | ./checker ${entries})
+	if [ "$test_pushswap" = "OK" ] ; then
+		printf "${GREEN}${test_pushswap}${NOCOLOR}"
+		nb_instructions=$(wc -l < tmp)
+		if (( "$nb_instructions" < ${limit[j][0]} )) ; then
+			echo "${GREEN}${nb_instructions}${NOCOLOR}"
+		elif (( "$nb_instructions" < ${limit[j][1]} )) ; then
+			echo "${ORANGE}${nb_instructions}${NOCOLOR}"
+		else
+			echo "${RED}${nb_instructions}${NOCOLOR}"
+		fi
+	elif [ "$test_pushswap" = "KO" ] ; then
+		echo "${RED}${test_pushswap}${NOCOLOR}"
+	fi
+	rm -f tmp
+	read -n 1 -s -p "
+Continue with same values? (y/n or q to quit)" var
+	if [ "$var" = "n" ]; then
+		clear
+		i=0
+		read -p "
+Enter MIN value: " MIN
+		read -p "
+Enter MAX value: " MAX
+		read -p "
+Enter RANGE value: " RANGE
+	elif [ "$var" = "q" ]; then
+		break
+	fi
+done
 
